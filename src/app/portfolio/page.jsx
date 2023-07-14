fa648dfb1e3de41568f3b29bda0db4e781122c71
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-07-14 17:01:37 +0300

    latest update

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './page.module.css'
+const Portfolio = () => {
+  return (
+    <div className={styles.classname}>
+      Portfolio
+    </div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './page.module.css'
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+        <div>©kiro 2023. All rights reserved</div>
+    </div>
+  )
+}
+
+export default Footer
